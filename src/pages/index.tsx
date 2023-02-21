f94a22a055b8047225478db61c27c60468bbe6b0
JPSS14 <jp_scarabelli@hotmail.com>
2023-02-21 09:39:05 -0300

    Add Description in Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Content } from '../components/content';
+import { Description } from '../components/description';
 
 export default function Home() {
   return (
@@ -11,7 +12,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Content></Content>
+        <Content>
+          <Description/>
+        </Content>
       </main>
     </>
   );
