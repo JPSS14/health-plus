cc13efb97d370235f7096ba54a00585a10109cca
JPSS14 <jp_scarabelli@hotmail.com>
2023-02-21 22:35:24 -0300

    Add Header in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app';
+import { Header } from '../components/header';
 import { HPlusThemeProvider } from '../components/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <HPlusThemeProvider>
+      <Header/>
       <Component {...pageProps} />
     </HPlusThemeProvider>
   </>
