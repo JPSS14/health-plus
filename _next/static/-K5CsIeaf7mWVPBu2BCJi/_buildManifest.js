5227047c7cec18c78f1a0ec69eb71bca431df295
JPSS14 <JPSS14@users.noreply.github.com>
2023-03-10 04:27:57 +0000

    deploy: 23f1c043063ea247b008002b4ef88e0b5a04875e

--- _next/static/-K5CsIeaf7mWVPBu2BCJi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/518-a02d4752c5579bd0.js","static/chunks/pages/index-81b37d58cc4bcee9.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/XlWzr7JFrlpEH0gz2I4Wc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/518-a02d4752c5579bd0.js","static/chunks/pages/index-ad04c76e79ba7b33.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
