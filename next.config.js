a8af6c2396633d305bcd680f4947ee8b7aab5266
JPSS14 <jp_scarabelli@hotmail.com>
2023-03-10 00:32:30 -0300

    Fix basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/health-plus/',
-  assetPrefix: '/health-plus/',
+  basePath: '/health-plus',
+  assetPrefix: '/health-plus',
   publicRuntimeConfig:{
     baseUrl: process.env.BASE_URL || 'http://localhost:3000/'
   }
