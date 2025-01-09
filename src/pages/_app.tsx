b3fed49c9aa6dc8d3f30ec5cb57cc29ef106a62e
JPSS14 <jp_scarabelli@hotmail.com>
2025-01-09 20:59:11 -0300

    style: add maxWidth in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import type { AppProps } from 'next/app';
-import { Header } from '../components/header';
-import { HPlusThemeProvider } from '../components/theme';
+import type { AppProps } from "next/app";
+import { Header } from "../components/header";
+import { HPlusThemeProvider } from "../components/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <HPlusThemeProvider>
-      <Header/>
-      <Component {...pageProps} />
-    </HPlusThemeProvider>
-  </>
-};
+  return (
+    <div style={{ maxWidth: "1536px", margin: "0 auto" }}>
+      <HPlusThemeProvider>
+        <Header />
+        <Component {...pageProps} />
+      </HPlusThemeProvider>
+    </div>
+  );
+}
